49f3d4e2e2eae83617566746647425c7350906b8
maslina524 <luk.kirill2804@gmail.com>
2026-07-19 18:44:43 +0300

    Added fn read_file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char* read_file(char* file_path);
 
 // gcc src/main.c -std=c17 -Wall -Wextra -Wpedantic -g -O0 -o out; ./out
 int main(int argc, char** argv) {
-    printf("Hello World!\n");
+    if (argc == 1) {
+        printf("help page\n");
+        exit(0);
+    }
+
+    char* file_path = argv[1];
+    char* buf = read_file(file_path);
+
+    printf("%s\n", buf);
+
     return 0;
+}
+
+char* read_file(char* file_path) {
+    FILE* file = fopen(file_path, "rb");
+    if (file == NULL) {
+        printf("failed to read file\n");
+        exit(1);
+    }
+
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file); 
+
+    char* buf = (char*)malloc(size + 1);
+    if (buf == NULL) {
+        printf("failed to allocate memory for buffer\n");
+        fclose(file);
+        exit(1);
+    }
+
+    fread(buf, 1, size, file);
+    buf[size] = '\0';
+
+    fclose(file);
+    return buf;
 }
